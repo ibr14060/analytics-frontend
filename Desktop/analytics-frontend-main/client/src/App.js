a24c26d4556c8e6aa519d1330309c8cd9fe429a8
ibr14060 <104881960+ibr14060@users.noreply.github.com>
2023-01-06 06:07:20 +0200

    allah yabny allah

--- Desktop/analytics-frontend-main/client/src/App.js
+++ Desktop/analytics-frontend-main/client/src/App.js
@@ -1,5 +1,6 @@
 
-import React from "react";
+import React, { useState, useEffect } from "react";
+//import React from "react";
 import "./App.css";
 
 import {
@@ -73,6 +74,8 @@ var count_of_BRA=0;
 var count_of_SRB=0;
 
 const configData = require('./data.json');
+
+
 //console.log(configData.length);
 //console.log(configData[0].stat);
 
@@ -91,106 +94,214 @@ if(configData[i].stat=="reserved"){
 
 }
 for( var j=0;j<configData.length;j++){
-    if(configData[j].homeTeam=="Qatar"||configData[j].awayTeam=="Qatar"){
+    if(configData[j].matchNumber==1&&configData[j].stat=="reserved"){
         count_of_QAT ++; 
-    }
-    if(configData[j].homeTeam=="Ecuador"||configData[j].awayTeam=="Ecuador"){
         count_of_ECU ++; 
     }
-    if(configData[j].homeTeam=="England"||configData[j].awayTeam=="England"){
-        count_of_ENG ++; 
+    if(configData[j].matchNumber==2 &&configData[j].stat=="reserved"){
+        count_of_SEN ++; 
+        count_of_NED ++;  
     }
-    if(configData[j].homeTeam=="Iran"||configData[j].awayTeam=="Iran"){
+    if(configData[j].matchNumber==3 &&configData[j].stat=="reserved"){
+        count_of_ENG ++; 
         count_of_IRN ++; 
     }
-    if(configData[j].homeTeam=="Senegal"||configData[j].awayTeam=="Senegal"){
-        count_of_SEN ++; 
-    }
-    if(configData[j].homeTeam=="Netherlands"||configData[j].awayTeam=="Netherlands"){
-        count_of_NED ++; 
-    }
-    if(configData[j].homeTeam=="USA"||configData[j].awayTeam=="USA"){
-        count_of_USA ++; 
-    }
-    if(configData[j].homeTeam=="Wales"||configData[j].awayTeam=="Wales"){
+    if(configData[j].matchNumber==4&&configData[j].stat=="reserved"){
+        count_of_USA ++;  
         count_of_WAL ++; 
     }
-    if(configData[j].homeTeam=="Argentina"||configData[j].awayTeam=="Argentina"){
-        count_of_ARG ++; 
-    }
-    if(configData[j].homeTeam=="Saudi Arabia"||configData[j].awayTeam=="Saudi Arabia"){
-        count_of_KSA ++; 
-    }
-    if(configData[j].homeTeam=="Denmark"||configData[j].awayTeam=="Denmark"){
-        count_of_DEN ++; 
-    }
-    if(configData[j].homeTeam=="Tunisia"||configData[j].awayTeam=="Tunisia"){
-        count_of_TUN ++; 
-    }
-    if(configData[j].homeTeam=="Mexico"||configData[j].awayTeam=="Mexico"){
-        count_of_MEX ++; 
-    }
-    if(configData[j].homeTeam=="Poland"||configData[j].awayTeam=="Poland"){
-        count_of_POL ++; 
-    }
-    if(configData[j].homeTeam=="France"||configData[j].awayTeam=="France"){
+    if(configData[j].matchNumber==5&&configData[j].stat=="reserved"){
+        count_of_AUS ++; 
         count_of_FRA ++; 
     }
-    if(configData[j].homeTeam=="Australia"||configData[j].awayTeam=="Australia"){
-        count_of_AUS ++; 
+    if(configData[j].matchNumber==6&&configData[j].stat=="reserved"){
+        count_of_TUN ++; 
+        count_of_DEN ++;  
     }
-    if(configData[j].homeTeam=="Morocco"||configData[j].awayTeam=="Morocco"){
-        count_of_MAR ++; 
+    if(configData[j].matchNumber==7&&configData[j].stat=="reserved"){
+        count_of_POL ++; 
+        count_of_MEX ++; 
     }
-    if(configData[j].homeTeam=="Croatia"||configData[j].awayTeam=="Croatia"){
-        count_of_CRO ++; 
+    if(configData[j].matchNumber==8&&configData[j].stat=="reserved"){
+        count_of_KSA ++; 
+        count_of_ARG ++; 
     }
-    if(configData[j].homeTeam=="Germany"||configData[j].awayTeam=="Germany"){
-        count_of_GER ++; 
+    if(configData[j].matchNumber==9&&configData[j].stat=="reserved"){
+        count_of_CAN ++; 
+        count_of_BEL ++;  
     }
-    if(configData[j].homeTeam=="Japan"||configData[j].awayTeam=="Japan"){
-        count_of_JPN ++; 
-    }
-    if(configData[j].homeTeam=="Spain"||configData[j].awayTeam=="Spain"){
+    if(configData[j].matchNumber==10&&configData[j].stat=="reserved"){
+        count_of_WRS ++; 
         count_of_ESP ++; 
     }
-    if(configData[j].homeTeam=="Costa Rica"||configData[j].awayTeam=="Costa Rica"){
-        count_of_WRS ++; 
+    if(configData[j].matchNumber==11&&configData[j].stat=="reserved"){
+        count_of_JPN ++; 
+        count_of_GER ++; 
     }
-    if(configData[j].homeTeam=="Belgium"||configData[j].awayTeam=="Belgium"){
-        count_of_BEL ++; 
+    if(configData[j].matchNumber==12&&configData[j].stat=="reserved"){
+        count_of_CRO ++; 
+        count_of_MAR ++; 
     }
-    if(configData[j].homeTeam=="Canada"||configData[j].awayTeam=="Canada"){
-        count_of_CAN ++; 
-    }
-    if(configData[j].homeTeam=="Switzerland"||configData[j].awayTeam=="Switzerland"){
+    if(configData[j].matchNumber==13&&configData[j].stat=="reserved"){
+        count_of_CMR ++;
         count_of_SUI ++; 
     }
-    if(configData[j].homeTeam=="Cameroon"||configData[j].awayTeam=="Cameroon"){
-        count_of_CMR ++; 
-    }
-    if(configData[j].homeTeam=="Uruguay"||configData[j].awayTeam=="Uruguay"){
+    if(configData[j].matchNumber==14&&configData[j].stat=="reserved"){
+        count_of_PRK ++; 
         count_of_URU ++; 
     }
-    if(configData[j].homeTeam=="Korea Republic"||configData[j].awayTeam=="Korea Republic"){
-        count_of_PRK ++; 
-    }
-    if(configData[j].homeTeam=="Portugal"||configData[j].awayTeam=="Portugal"){
-         count_of_POR++; 
-    }
-    if(configData[j].homeTeam=="Ghana"||configData[j].awayTeam=="Ghana"){
+    if(configData[j].matchNumber==15&&configData[j].stat=="reserved"){
         count_of_GHA ++; 
+        count_of_POR++; 
     }
-    if(configData[j].homeTeam=="Brazil"||configData[j].awayTeam=="Brazil"){
+    if(configData[j].matchNumber==16&&configData[j].stat=="reserved"){
+        count_of_SRB ++; 
         count_of_BRA ++; 
     }
-    if(configData[j].homeTeam=="Serbia"||configData[j].awayTeam=="Serbia"){
-        count_of_res ++; 
+    if(configData[j].matchNumber==17&&configData[j].stat=="reserved"){
+        count_of_WAL ++;
+        count_of_IRN ++;  
+    }
+    if(configData[j].matchNumber==18&&configData[j].stat=="reserved"){
+        count_of_SEN ++; 
+        count_of_QAT ++;  
+    }
+    if(configData[j].matchNumber==19&&configData[j].stat=="reserved"){
+        count_of_ECU ++; 
+        count_of_NED ++; 
+    }
+    if(configData[j].matchNumber==20&&configData[j].stat=="reserved"){
+        count_of_ENG ++; 
+        count_of_USA ++; 
+    }
+    if(configData[j].matchNumber==21&&configData[j].stat=="reserved"){
+        count_of_TUN ++; 
+        count_of_AUS ++; 
+    }
+    if(configData[j].matchNumber==22&&configData[j].stat=="reserved"){
+        count_of_POL ++; 
+        count_of_KSA ++;
+    }
+    if(configData[j].matchNumber==23&&configData[j].stat=="reserved"){
+        count_of_DEN ++;
+        count_of_FRA ++;
+    }
+    if(configData[j].matchNumber==24&&configData[j].stat=="reserved"){
+        count_of_ARG ++; 
+        count_of_MEX ++; 
+    }
+    if(configData[j].matchNumber==25&&configData[j].stat=="reserved"){
+        count_of_JPN ++;  
+        count_of_WRS ++; 
+    }
+    if(configData[j].matchNumber==26&&configData[j].stat=="reserved"){
+        count_of_BEL ++; 
+        count_of_MAR ++;
+    }
+    if(configData[j].matchNumber==27 &&configData[j].stat=="reserved"){
+        count_of_CRO ++; 
+        count_of_CAN ++;
+    }
+    if(configData[j].matchNumber==28&&configData[j].stat=="reserved"){
+        count_of_ESP ++; 
+        count_of_GER ++; 
+    }
+    if(configData[j].matchNumber==29&&configData[j].stat=="reserved"){
+        count_of_CMR++; 
+        count_of_SRB++; 
+    }
+    if(configData[j].matchNumber==30&&configData[j].stat=="reserved"){
+        count_of_GHA ++; 
+        count_of_PRK++; 
+    }
+    if(configData[j].matchNumber==31&&configData[j].stat=="reserved"){
+        count_of_BRA ++;
+        count_of_SUI ++;
+    }
+    if(configData[j].matchNumber==32&&configData[j].stat=="reserved"){
+        count_of_POR ++; 
+        count_of_URU ++; 
     }   
+    if(configData[j].matchNumber==33&&configData[j].stat=="reserved"){
+        count_of_ENG ++; 
+        count_of_WAL ++;
+    }
+    if(configData[j].matchNumber==34&&configData[j].stat=="reserved"){
+        count_of_USA ++;
+        count_of_IRN ++;  
+    }
+    if(configData[j].matchNumber==35&&configData[j].stat=="reserved"){
+        count_of_SEN ++; 
+        count_of_ECU ++; 
+    }
+    if(configData[j].matchNumber==36&&configData[j].stat=="reserved"){
+        count_of_QAT ++; 
+        count_of_NED ++; 
+    }
+    if(configData[j].matchNumber==37&&configData[j].stat=="reserved"){
+        count_of_DEN ++;
+        count_of_AUS ++;  
+    }
+    if(configData[j].matchNumber==38&&configData[j].stat=="reserved"){
+        count_of_FRA ++; 
+        count_of_TUN ++; 
+    }
+    if(configData[j].matchNumber==39&&configData[j].stat=="reserved"){
+        count_of_POL ++; 
+        count_of_ARG ++; 
+    }
+    if(configData[j].matchNumber==40&&configData[j].stat=="reserved"){
+        count_of_KSA ++; 
+        count_of_MEX ++; 
+    }
+    if(configData[j].matchNumber==41&&configData[j].stat=="reserved"){
+        count_of_BEL ++; 
+        count_of_CRO ++; 
+    }
+    if(configData[j].matchNumber==42&&configData[j].stat=="reserved"){
+        count_of_CAN ++; 
+        count_of_MAR ++; 
+    }
+    if(configData[j].matchNumber==43&&configData[j].stat=="reserved"){
+        count_of_JPN ++; 
+        count_of_ESP ++; 
+    }
+    if(configData[j].matchNumber==44&&configData[j].stat=="reserved"){
+        count_of_GER ++; 
+        count_of_WRS ++;
+    }
+    if(configData[j].matchNumber==45&&configData[j].stat=="reserved"){
+        count_of_URU ++; 
+        count_of_GHA ++;
+    }
+    if(configData[j].matchNumber==46&&configData[j].stat=="reserved"){
+        count_of_POR ++; 
+        count_of_PRK ++;
+    }
+    if(configData[j].matchNumber==47&&configData[j].stat=="reserved"){
+        count_of_SRB ++; 
+        count_of_SUI ++;
+    }
+    if(configData[j].matchNumber==48&&configData[j].stat=="reserved"){
+        count_of_BRA ++;
+        count_of_CMR ++;  
+    }
+    
 }
 //console.log(count_of_res);
 
 const App = () => {
+
+    
+
+/*
+return (
+<div className="App">
+    <h1>{message}</h1>
+</div>
+);
+}
+*/
  //   var x=40;
 const data = [
 { stat: "reserved", value: count_of_res },
@@ -200,201 +311,202 @@ const data = [
 ];
 
 const dataofmatches = [
-{
-    name: "QAT",
-    uv: count_of_QAT,
-    pv: 2400,
-    amt: 2400
-},
-{
-    name: "ECU",
-    uv: count_of_ECU,
-    pv: 1398,
-    amt: 2210
-},
-{
-    name: "ENG",
-    uv: count_of_ENG,
-    pv: 9800,
-    amt: 2290
-},
-{
-    name: "IRN",
-    uv: count_of_IRN,
-    pv: 3908,
-    amt: 2000
-},
-{
-    name: "SEN",
-    uv: count_of_SEN,
-    pv: 4800,
-    amt: 2181
-},
-{
-    name: "NED",
-    uv: count_of_NED,
-    pv: 3800,
-    amt: 2500
-},
-{
-    name: "USA",
-    uv: count_of_USA,
-    pv: 4300,
-    amt: 2100
-},
-{
-    name: "WAL",
-    uv: count_of_WAL,
-    pv: 2400,
-    amt: 2400
-},
-{
-    name: "ARG",
-    uv: count_of_ARG,
-    pv: 100008,
-    amt: 2210
-},
-{
-    name: "KSA",
-    uv: count_of_KSA,
-    pv: 9800,
-    amt: 2290
-},
-{
-    name: "DEN",
-    uv: count_of_DEN,
-    pv: 3908,
-    amt: 2000
-},
-{
-    name: "TUN",
-    uv: count_of_TUN,
-    pv: 4800,
-    amt: 2181
-},
-{
-    name: "MEX",
-    uv: count_of_MEX,
-    pv: 3800,
-    amt: 2500
-},
-{
-    name: "POL",
-    uv: count_of_POL,
-    pv: 4300,
-    amt: 2100
-},
-{
-    name: "FRA",
-    uv: count_of_FRA,
-    pv: 2400,
-    amt: 2400
-},
-{
-    name: "AUS",
-    uv: count_of_AUS,
-    pv: 1398,
-    amt: 2210
-},
-{
-    name: "MAR",
-    uv: count_of_MAR,
-    pv: 9800,
-    amt: 2290
-},
-{
-    name: "CRO",
-    uv: count_of_CRO,
-    pv: 3908,
-    amt: 2000
-},
-{
-    name: "GER",
-    uv: count_of_GER,
-    pv: 4800,
-    amt: 2181
-},
-{
-    name: "JPN",
-    uv: count_of_JPN,
-    pv: 3800,
-    amt: 2500
-},
-{
-    name: "ESP",
-    uv: count_of_ESP,
-    pv: 4300,
-    amt: 2100
-},
-{
-    name: "WRs",
-    uv: count_of_WRS,
-    pv: 2400,
-    amt: 2400
-},
-{
-    name: "BEL",
-    uv: count_of_BEL,
-    pv: 100008,
-    amt: 2210
-},
-{
-    name: "CAN",
-    uv: count_of_CAN,
-    pv: 9800,
-    amt: 2290
-},
-{
-    name: "SUI",
-    uv: count_of_SUI,
-    pv: 3908,
-    amt: 2000
-},
-{
-    name: "CMR",
-    uv: count_of_CMR,
-    pv: 4800,
-    amt: 2181
-},
-{
-    name: "URU",
-    uv: count_of_URU,
-    pv: 3800,
-    amt: 2500
-},
-{
-    name: "PRK",
-    uv: count_of_PRK,
-    pv: 4300,
-    amt: 2100
-},
-{
-    name: "POR",
-    uv: count_of_POR,
-    pv: 9800,
-    amt: 2290
-},
-{
-    name: "GHA",
-    uv: count_of_GHA,
-    pv: 3908,
-    amt: 2000
-},
-{
-    name: "BRA",
-    uv: count_of_BRA,
-    pv: 4800,
-    amt: 2181
-},
-{
-    name: "SRB",
-    uv: count_of_res,
-    pv: 3800,
-    amt: 2500
-},
 
-
-];
+    {
+        name: "QAT",
+        uv: count_of_QAT,
+        pv: 2400,
+        amt: 2400
+    },
+    {
+        name: "ECU",
+        uv: count_of_ECU,
+        pv: 1398,
+        amt: 2210
+    },
+    {
+        name: "ENG",
+        uv: count_of_ENG,
+        pv: 9800,
+        amt: 2290
+    },
+    {
+        name: "IRN",
+        uv: count_of_IRN,
+        pv: 3908,
+        amt: 2000
+    },
+    {
+        name: "SEN",
+        uv: count_of_SEN,
+        pv: 4800,
+        amt: 2181
+    },
+    {
+        name: "NED",
+        uv: count_of_NED,
+        pv: 3800,
+        amt: 2500
+    },
+    {
+        name: "USA",
+        uv: count_of_USA,
+        pv: 4300,
+        amt: 2100
+    },
+    {
+        name: "WAL",
+        uv: count_of_WAL,
+        pv: 2400,
+        amt: 2400
+    },
+    {
+        name: "ARG",
+        uv: count_of_ARG,
+        pv: 100008,
+        amt: 2210
+    },
+    {
+        name: "KSA",
+        uv: count_of_KSA,
+        pv: 9800,
+        amt: 2290
+    },
+    {
+        name: "DEN",
+        uv: count_of_DEN,
+        pv: 3908,
+        amt: 2000
+    },
+    {
+        name: "TUN",
+        uv: count_of_TUN,
+        pv: 4800,
+        amt: 2181
+    },
+    {
+        name: "MEX",
+        uv: count_of_MEX,
+        pv: 3800,
+        amt: 2500
+    },
+    {
+        name: "POL",
+        uv: count_of_POL,
+        pv: 4300,
+        amt: 2100
+    },
+    {
+        name: "FRA",
+        uv: count_of_FRA,
+        pv: 2400,
+        amt: 2400
+    },
+    {
+        name: "AUS",
+        uv: count_of_AUS,
+        pv: 1398,
+        amt: 2210
+    },
+    {
+        name: "MAR",
+        uv: count_of_MAR,
+        pv: 9800,
+        amt: 2290
+    },
+    {
+        name: "CRO",
+        uv: count_of_CRO,
+        pv: 3908,
+        amt: 2000
+    },
+    {
+        name: "GER",
+        uv: count_of_GER,
+        pv: 4800,
+        amt: 2181
+    },
+    {
+        name: "JPN",
+        uv: count_of_JPN,
+        pv: 3800,
+        amt: 2500
+    },
+    {
+        name: "ESP",
+        uv: count_of_ESP,
+        pv: 4300,
+        amt: 2100
+    },
+    {
+        name: "WRs",
+        uv: count_of_WRS,
+        pv: 2400,
+        amt: 2400
+    },
+    {
+        name: "BEL",
+        uv: count_of_BEL,
+        pv: 100008,
+        amt: 2210
+    },
+    {
+        name: "CAN",
+        uv: count_of_CAN,
+        pv: 9800,
+        amt: 2290
+    },
+    {
+        name: "SUI",
+        uv: count_of_SUI,
+        pv: 3908,
+        amt: 2000
+    },
+    {
+        name: "CMR",
+        uv: count_of_CMR,
+        pv: 4800,
+        amt: 2181
+    },
+    {
+        name: "URU",
+        uv: count_of_URU,
+        pv: 3800,
+        amt: 2500
+    },
+    {
+        name: "PRK",
+        uv: count_of_PRK,
+        pv: 4300,
+        amt: 2100
+    },
+    {
+        name: "POR",
+        uv: count_of_POR,
+        pv: 9800,
+        amt: 2290
+    },
+    {
+        name: "GHA",
+        uv: count_of_GHA,
+        pv: 3908,
+        amt: 2000
+    },
+    {
+        name: "BRA",
+        uv: count_of_BRA,
+        pv: 4800,
+        amt: 2181
+    },
+    {
+        name: "SRB",
+        uv: count_of_SRB,
+        pv: 3800,
+        amt: 2500
+    },
+    
+    
+    ];
 const colors = scaleOrdinal(schemeCategory10).range();
 const COLORS = ['#8f8e8e', '#dec672',' #8d0a30'];
 return (
